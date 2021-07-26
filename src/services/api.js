3527dfe69a5f4100dfa6a71cc509d8669e667070
samenc99 <mateus.enc@gmail.com>
2021-07-26 12:21:56 -0300

    mudando url de servidor do back

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://portfolio-samuel.herokuapp.com",
+  baseURL: "http://ec2-3-85-39-7.compute-1.amazonaws.com:3003",
 });
